c3cf873e82808e9e603d8ebe0b1f37c1761c7761
cristian <cristian.silva@agriness.com>
2023-03-23 17:59:45 -0300

    Adding comments and explanations

--- 01-Big-O/O(n^2).py
+++ 01-Big-O/O(n^2).py
@@ -1,4 +1,9 @@
 def print_items(n):
+    '''
+        The below is the equivalent Big O: n*n=n² = O(n²)
+        It does not matter if it's O(n) to the 3rd, 4th, 10th, we always simply it as O(n²)
+        It means it is a lot less efficient from a time complexity standpoint
+    '''
     for i in range(n):
         for j in range(n):
             print(i,j) 
